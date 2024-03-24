7c4a706cb0736186fd639df37db51be71df465fc
arjun2004 <arjunajju2004@gmail.com>
2024-03-24 17:01:46 +0530

    more

--- REACT/website/src/App.jsx
+++ REACT/website/src/App.jsx
@@ -1,34 +1,30 @@
-import Card from './card.jsx'
-import RegistrationForm from './form.jsx'
-import food from './food.jsx'
-import List from './List.jsx'
-import profile from './assets/WhatsApp Image 2024-02-25 at 21.07.46_26f4386f.jpg'
-import profile1 from './assets/MY PHOTO 2.jpg'
-import { useState } from 'react'
-import React from 'react';
-import ReactDOM from 'react-dom';
+import Card from "./card.jsx";
+import RegistrationForm from "./form.jsx";
+import List from "./List.jsx";
+import profile from "./assets/WhatsApp Image 2024-02-25 at 21.07.46_26f4386f.jpg";
+import profile1 from "./assets/MY PHOTO 2.jpg";
+import { useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
+
 // import App from './App';
-import './index.css';
-import "./assets/fonts/regular.ttf"
-import "./assets/fonts/bold.ttf"
+import "./index.css";
+import "./assets/fonts/regular.ttf";
+import "./assets/fonts/bold.ttf";
 function App() {
-    return (
-      <div className="App">
-        <RegistrationForm />
-     
-      </div>
-    );
-  }
-export default App
-
-
-
+  return (
+    <div className="App">
+      <RegistrationForm />
+    </div>
+  );
+}
+export default App;
 
 // #git init
 // # git commit -m "first commit"
 // # git branch -M main
 // # git push -u origin main
-
 
 // #####
 // # git init
--- REACT/website/src/main.jsx
+++ REACT/website/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "./food.jsx";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "food/",
+    element: <Food />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
